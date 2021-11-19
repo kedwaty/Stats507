f75a79d3a7c7cd02c47fdda410e6aeeb0e897100
youngwoo Kwon (young) <79286837+kedwaty@users.noreply.github.com>
2021-11-19 13:07:23 -0500

    Update pd_topic_group2_3d_kedwaty.py

--- pandas_notes/pd_topic_group2_3d_kedwaty.py
+++ pandas_notes/pd_topic_group2_3d_kedwaty.py
@@ -1,6 +1,76 @@
 # ---
 # jupyter:
 #   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.12.0
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# ## **Name**  : *Xin Luo*
+# ## **EMAIL**  :  *luosanj@umich.edu*
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# #  Question 0
+#
+# ##  Pandas .interpolate() method
+#
+# * Method *interpolate* is very useful to fill NaN values.
+# * By default, NaN values can be filled by other values with the same index for different methods.
+# * Please note that NaN values in DataFrame/Series with MultiIndex can be filled by 'linear' method as
+# <code>method = 'linear' </code>. 
+
+# + slideshow={"slide_type": "subslide"}
+import pandas as pd
+import numpy as np
+a = pd.DataFrame({'a' : [1, 2, np.nan, 5], 'b' : [4, np.nan, 6, 8]})
+a.interpolate(method = 'linear')
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# ### Parameters in .interpolate()
+# ##### *parameter **'method'** : *str*, default *'linear'
+#
+#
+# * Most commonly used methods:
+#     * 1. **'linear'** : linear regression mind to fit the missing ones.
+#     * 2. **'pad', 'limit'** :  Fill in NaNs using existing values. Note:Interpolation through padding means copying the value just before a missing entry.While using padding interpolation, you need to specify a limit. The limit is the maximum number of nans the method can fill consecutively.
+#     * 3. **'polynomial', 'order'** : Polynomial regression mind with a set order to fit the missing ones. Note : NaN of the first column remains, because there is no entry before it to use for interpolation.
+
+# + slideshow={"slide_type": "subslide"}
+m =  pd.Series([0, 1, np.nan, np.nan, 3, 5, 8])
+m.interpolate(method = 'pad', limit = 2)
+
+# + slideshow={"slide_type": "subslide"}
+n = pd.Series([10, 2, np.nan, 4, np.nan, 3, 2, 6]) 
+n.interpolate(method = 'polynomial', order = 2)
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# ##### parameter **'axis'** :  default *None*
+# * 1. axis = 0 : Axis to interpolate along is index.
+# * 2. axis = 1 : Axis to interpolate along is column.
+#     
+
+# + slideshow={"slide_type": "subslide"}
+k = pd.DataFrame({'a' : [1, 2, np.nan, 5], 'b' : [4, np.nan, 6, 8]})
+k.interpolate(method = 'linear', axis = 0)
+k.interpolate(method = 'linear', axis = 1)
+
+# + [markdown] slideshow={"slide_type": "slide"}
+# ###  Returns
+# * Series or DataFrame or None
+# * Returns the same object type as the caller, interpolated at some or all NaN values or None if `inplace=True`.
+# -
+
+# ---
+# jupyter:
+#   jupytext:
 #     cell_metadata_json: true
 #     notebook_metadata_filter: markdown
 #     text_representation:
@@ -80,75 +150,6 @@ idx[:5] + pd.offsets.Day(2)
 
 idx[:5] + pd.offsets.Minute(1)
 
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py:light
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.12.0
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
-# + [markdown] slideshow={"slide_type": "slide"}
-# ## **Name**  : *Xin Luo*
-# ## **EMAIL**  :  *luosanj@umich.edu*
-
-# + [markdown] slideshow={"slide_type": "slide"}
-# #  Question 0
-#
-# ##  Pandas .interpolate() method
-#
-# * Method *interpolate* is very useful to fill NaN values.
-# * By default, NaN values can be filled by other values with the same index for different methods.
-# * Please note that NaN values in DataFrame/Series with MultiIndex can be filled by 'linear' method as
-# <code>method = 'linear' </code>. 
-
-# + slideshow={"slide_type": "subslide"}
-import pandas as pd
-import numpy as np
-a = pd.DataFrame({'a' : [1, 2, np.nan, 5], 'b' : [4, np.nan, 6, 8]})
-a.interpolate(method = 'linear')
-
-# + [markdown] slideshow={"slide_type": "slide"}
-# ### Parameters in .interpolate()
-# ##### *parameter **'method'** : *str*, default *'linear'
-#
-#
-# * Most commonly used methods:
-#     * 1. **'linear'** : linear regression mind to fit the missing ones.
-#     * 2. **'pad', 'limit'** :  Fill in NaNs using existing values. Note:Interpolation through padding means copying the value just before a missing entry.While using padding interpolation, you need to specify a limit. The limit is the maximum number of nans the method can fill consecutively.
-#     * 3. **'polynomial', 'order'** : Polynomial regression mind with a set order to fit the missing ones. Note : NaN of the first column remains, because there is no entry before it to use for interpolation.
-
-# + slideshow={"slide_type": "subslide"}
-m =  pd.Series([0, 1, np.nan, np.nan, 3, 5, 8])
-m.interpolate(method = 'pad', limit = 2)
-
-# + slideshow={"slide_type": "subslide"}
-n = pd.Series([10, 2, np.nan, 4, np.nan, 3, 2, 6]) 
-n.interpolate(method = 'polynomial', order = 2)
-
-# + [markdown] slideshow={"slide_type": "slide"}
-# ##### parameter **'axis'** :  default *None*
-# * 1. axis = 0 : Axis to interpolate along is index.
-# * 2. axis = 1 : Axis to interpolate along is column.
-#     
-
-# + slideshow={"slide_type": "subslide"}
-k = pd.DataFrame({'a' : [1, 2, np.nan, 5], 'b' : [4, np.nan, 6, 8]})
-k.interpolate(method = 'linear', axis = 0)
-k.interpolate(method = 'linear', axis = 1)
-
-# + [markdown] slideshow={"slide_type": "slide"}
-# ###  Returns
-# * Series or DataFrame or None
-# * Returns the same object type as the caller, interpolated at some or all NaN values or None if `inplace=True`.
-# -
 
 #!/usr/bin/env python
 # coding: utf-8
@@ -238,4 +239,3 @@ print(type(df2.iloc[1,1]))
 
 
 pd.Series([1, 2, np.nan, 4], dtype=pd.Int64Dtype())
-
